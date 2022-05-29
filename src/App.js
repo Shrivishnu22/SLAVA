b971671a8038de8421f8f3fd98c54feb06b06414
Shrivishnu22 <akshyavishnu@gmail.com>
2022-05-30 00:01:31 +0530

    Add NavBar and Its Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
+import Home from "./components/Home";
+import Task from "./components/Task";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Signup from "./components/Signup";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
-    <div className="App">
-      <h1>Slava</h1>
-      <p>Created by Shri Somu</p>      
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/task" element={<Task/>} />
+        <Route exact path="/contact" element={<Contact/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/signup" element={<Signup/>}/>
+      </Routes>
+    </Router>    
   );
 }
 
